e9490d20def0b3d6aa7e38982ae90162b5039b23
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-02-28 22:34:39 -0500

    flex view is added

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Image, Linking, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState({ articles: [] });
+  console.log(data);
+
+  const refreshUser = () => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=Apple&from=2022-02-28&sortBy=popularity&apiKey=bb9cb5ef52124cd69c5c0b1490bacc45"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  };
+  useEffect(refreshUser, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {data.articles.map((news, index) => (
+        <View key={index} style={styles.flex}>
+          <Image
+            style={styles.userImage}
+            source={{
+              uri: news.urlToImage,
+            }}
+          />
+          <Text>{new Date(news.publishedAt).toDateString()}</Text>
+          <View>
+            <Text
+              style={{ color: "blue" }}
+              target="target_blank"
+              onPress={() => Linking.openURL(news.url)}
+            >
+              <Text>{news.title}</Text>
+            </Text>
+            <Text>{news.description}</Text>
+          </View>
+        </View>
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    margin: 20,
+  },
+  flex: {
+    width: 400,
+    height: 500,
+  },
+  userImage: {
+    margin: 25,
+    width: 200,
+    height: 200,
   },
 });
